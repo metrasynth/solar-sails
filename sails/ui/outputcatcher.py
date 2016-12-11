cf8909496e9ecae2059978c58f7de37632e419a3
Matthew Scott <matt@11craft.com>
2016-12-11 17:42:25 -0600

    Scroll to bottom of output after running script

--- sails/ui/outputcatcher.py
+++ sails/ui/outputcatcher.py
@@ -5,18 +5,21 @@ from PyQt5.QtWidgets import QPlainTextEdit, qApp
 
 class OutputCatcher(object):
 
-    def __init__(self, editor):
+    def __init__(self, editor, keep_original=False):
         """
         :type editor: QPlainTextEdit
         """
         self.editor = editor
+        self.keep_original = keep_original
 
     def __enter__(self):
         self.editor.clear()
         self._orig_stdout = sys.stdout
         self._orig_stderr = sys.stderr
-        sys.stdout = OutputWriter(self.editor, sys.stdout)
-        sys.stderr = OutputWriter(self.editor, sys.stderr)
+        sys.stdout = OutputWriter(
+            self.editor, sys.stdout if self.keep_original else None)
+        sys.stderr = OutputWriter(
+            self.editor, sys.stderr if self.keep_original else None)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -30,10 +33,7 @@ class OutputWriter(object):
         self.editor = editor
         self.file = file
 
-    def flush(self):
-        self.file.flush()
-        qApp.processEvents()
-
     def write(self, s):
-        self.file.write(s)
+        self.file.write(s) if self.file else None
         self.editor.insertPlainText(s)
+        self.editor.ensureCursorVisible()
